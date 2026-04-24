7c7864f6cb1a3ca09db17316e607a26b4790bae0
salehuddiniwan <m.salehuddin@student.iium.edu.my>
2026-04-24 10:21:42 +0800

    Updated

--- config.py
+++ config.py
@@ -64,7 +64,7 @@ class Config:
     # Twilio WhatsApp
     TWILIO_ACCOUNT_SID = os.getenv("TWILIO_ACCOUNT_SID", "")
     TWILIO_AUTH_TOKEN = os.getenv("TWILIO_AUTH_TOKEN", "")
-    TWILIO_WHATSAPP_FROM = os.getenv("TWILIO_WHATSAPP_FROM", "whatsapp:+14155238886")
+    TWILIO_WHATSAPP_FROM = os.getenv("TWILIO_WHATSAPP_FROM", "whatsapp:+1122334455")
     ALERT_PHONE_NUMBERS = [
         p.strip() for p in os.getenv("ALERT_PHONE_NUMBERS", "").split(",") if p.strip()
     ]
